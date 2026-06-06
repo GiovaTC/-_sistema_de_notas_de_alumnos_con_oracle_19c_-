ea3a5b30ef965dea83f277bb16b055163bc751cd
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-06 13:32:12 -0500

    commit - Form1.Designer .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,263 +15,266 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
+            dgvDatos = new DataGridView();
             label1 = new Label();
-            textBox1 = new TextBox();
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
             label5 = new Label();
             label6 = new Label();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
-            textBox6 = new TextBox();
             label7 = new Label();
             label8 = new Label();
-            textBox7 = new TextBox();
-            textBox8 = new TextBox();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            txtNombre = new TextBox();
+            txtNota1 = new TextBox();
+            txtNota2 = new TextBox();
+            txtNota3 = new TextBox();
+            txtNota4 = new TextBox();
+            txtNota5 = new TextBox();
+            txtSuma = new TextBox();
+            txtPromedio = new TextBox();
+            btnCalcular = new Button();
+            btnGuardar = new Button();
+            btnConsultar = new Button();
+            btnLimpiar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).BeginInit();
             SuspendLayout();
             // 
-            // dataGridView1
+            // dgvDatos
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(49, 324);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(588, 114);
-            dataGridView1.TabIndex = 0;
+            dgvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDatos.Location = new Point(25, 330);
+            dgvDatos.Name = "dgvDatos";
+            dgvDatos.Size = new Size(740, 180);
+            dgvDatos.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(63, 22);
+            label1.Location = new Point(30, 25);
             label1.Name = "label1";
             label1.Size = new Size(103, 15);
             label1.TabIndex = 1;
             label1.Text = "Nombre Alumno :";
             // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(172, 19);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(215, 23);
-            textBox1.TabIndex = 2;
-            // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(49, 58);
+            label2.Location = new Point(30, 65);
             label2.Name = "label2";
-            label2.Size = new Size(39, 15);
-            label2.TabIndex = 3;
-            label2.Text = "Nota1";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Nota 1";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(49, 90);
+            label3.Location = new Point(30, 95);
             label3.Name = "label3";
-            label3.Size = new Size(39, 15);
-            label3.TabIndex = 4;
-            label3.Text = "Nota2";
+            label3.Size = new Size(42, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Nota 2";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(50, 122);
+            label4.Location = new Point(30, 125);
             label4.Name = "label4";
-            label4.Size = new Size(39, 15);
-            label4.TabIndex = 5;
-            label4.Text = "Nota3";
+            label4.Size = new Size(42, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Nota 3";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(50, 154);
+            label5.Location = new Point(30, 155);
             label5.Name = "label5";
-            label5.Size = new Size(39, 15);
-            label5.TabIndex = 6;
-            label5.Text = "Nota4";
+            label5.Size = new Size(42, 15);
+            label5.TabIndex = 5;
+            label5.Text = "Nota 4";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(50, 188);
+            label6.Location = new Point(30, 185);
             label6.Name = "label6";
-            label6.Size = new Size(39, 15);
-            label6.TabIndex = 7;
-            label6.Text = "Nota5";
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(153, 55);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(215, 23);
-            textBox2.TabIndex = 8;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(153, 87);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(215, 23);
-            textBox3.TabIndex = 9;
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(153, 122);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(215, 23);
-            textBox4.TabIndex = 10;
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(153, 154);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(215, 23);
-            textBox5.TabIndex = 11;
-            // 
-            // textBox6
-            // 
-            textBox6.Location = new Point(153, 188);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(215, 23);
-            textBox6.TabIndex = 12;
+            label6.Size = new Size(42, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Nota 5";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(52, 233);
+            label7.Location = new Point(30, 225);
             label7.Name = "label7";
             label7.Size = new Size(40, 15);
-            label7.TabIndex = 13;
+            label7.TabIndex = 7;
             label7.Text = "SUMA";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(52, 262);
+            label8.Location = new Point(30, 260);
             label8.Name = "label8";
             label8.Size = new Size(67, 15);
-            label8.TabIndex = 14;
+            label8.TabIndex = 8;
             label8.Text = "PROMEDIO";
             // 
-            // textBox7
+            // txtNombre
             // 
-            textBox7.Location = new Point(153, 230);
-            textBox7.Name = "textBox7";
-            textBox7.Size = new Size(215, 23);
-            textBox7.TabIndex = 15;
+            txtNombre.Location = new Point(150, 22);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(250, 23);
+            txtNombre.TabIndex = 1;
             // 
-            // textBox8
+            // txtNota1
             // 
-            textBox8.Location = new Point(153, 259);
-            textBox8.Name = "textBox8";
-            textBox8.Size = new Size(215, 23);
-            textBox8.TabIndex = 16;
+            txtNota1.Location = new Point(150, 62);
+            txtNota1.Name = "txtNota1";
+            txtNota1.Size = new Size(120, 23);
+            txtNota1.TabIndex = 2;
             // 
-            // button1
+            // txtNota2
             // 
-            button1.Location = new Point(49, 297);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 17;
-            button1.Text = "calcular";
-            button1.UseVisualStyleBackColor = true;
+            txtNota2.Location = new Point(150, 92);
+            txtNota2.Name = "txtNota2";
+            txtNota2.Size = new Size(120, 23);
+            txtNota2.TabIndex = 3;
             // 
-            // button2
+            // txtNota3
             // 
-            button2.Location = new Point(153, 297);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 18;
-            button2.Text = "guardar";
-            button2.UseVisualStyleBackColor = true;
+            txtNota3.Location = new Point(150, 122);
+            txtNota3.Name = "txtNota3";
+            txtNota3.Size = new Size(120, 23);
+            txtNota3.TabIndex = 4;
             // 
-            // button3
+            // txtNota4
             // 
-            button3.Location = new Point(254, 297);
-            button3.Name = "button3";
-            button3.Size = new Size(75, 23);
-            button3.TabIndex = 19;
-            button3.Text = "consultar";
-            button3.UseVisualStyleBackColor = true;
+            txtNota4.Location = new Point(150, 152);
+            txtNota4.Name = "txtNota4";
+            txtNota4.Size = new Size(120, 23);
+            txtNota4.TabIndex = 5;
             // 
-            // button4
+            // txtNota5
             // 
-            button4.Location = new Point(359, 297);
-            button4.Name = "button4";
-            button4.Size = new Size(75, 23);
-            button4.TabIndex = 20;
-            button4.Text = "limpiar";
-            button4.UseVisualStyleBackColor = true;
+            txtNota5.Location = new Point(150, 182);
+            txtNota5.Name = "txtNota5";
+            txtNota5.Size = new Size(120, 23);
+            txtNota5.TabIndex = 6;
+            // 
+            // txtSuma
+            // 
+            txtSuma.Location = new Point(150, 222);
+            txtSuma.Name = "txtSuma";
+            txtSuma.ReadOnly = true;
+            txtSuma.Size = new Size(120, 23);
+            txtSuma.TabIndex = 7;
+            // 
+            // txtPromedio
+            // 
+            txtPromedio.Location = new Point(150, 257);
+            txtPromedio.Name = "txtPromedio";
+            txtPromedio.ReadOnly = true;
+            txtPromedio.Size = new Size(120, 23);
+            txtPromedio.TabIndex = 8;
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(30, 289);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(120, 35);
+            btnCalcular.TabIndex = 9;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(165, 289);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(120, 35);
+            btnGuardar.TabIndex = 10;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            btnConsultar.Location = new Point(304, 289);
+            btnConsultar.Name = "btnConsultar";
+            btnConsultar.Size = new Size(120, 35);
+            btnConsultar.TabIndex = 11;
+            btnConsultar.Text = "Consultar";
+            btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new Point(439, 289);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(120, 35);
+            btnLimpiar.TabIndex = 12;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(textBox8);
-            Controls.Add(textBox7);
-            Controls.Add(label8);
-            Controls.Add(label7);
-            Controls.Add(textBox6);
-            Controls.Add(textBox5);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(textBox1);
+            ClientSize = new Size(800, 540);
+            Controls.Add(dgvDatos);
             Controls.Add(label1);
-            Controls.Add(dataGridView1);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(txtNombre);
+            Controls.Add(txtNota1);
+            Controls.Add(txtNota2);
+            Controls.Add(txtNota3);
+            Controls.Add(txtNota4);
+            Controls.Add(txtNota5);
+            Controls.Add(txtSuma);
+            Controls.Add(txtPromedio);
+            Controls.Add(btnCalcular);
+            Controls.Add(btnGuardar);
+            Controls.Add(btnConsultar);
+            Controls.Add(btnLimpiar);
             Name = "Form1";
-            Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Sistema de Notas - Oracle 19c";
+            Load += Form1_Load_1;
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private DataGridView dataGridView1;
+        private DataGridView dgvDatos;
+
         private Label label1;
-        private TextBox textBox1;
         private Label label2;
         private Label label3;
         private Label label4;
         private Label label5;
         private Label label6;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
-        private TextBox textBox6;
         private Label label7;
         private Label label8;
-        private TextBox textBox7;
-        private TextBox textBox8;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
+
+        private TextBox txtNombre;
+        private TextBox txtNota1;
+        private TextBox txtNota2;
+        private TextBox txtNota3;
+        private TextBox txtNota4;
+        private TextBox txtNota5;
+        private TextBox txtSuma;
+        private TextBox txtPromedio;
+
+        private Button btnCalcular;
+        private Button btnGuardar;
+        private Button btnConsultar;
+        private Button btnLimpiar;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,27 @@ namespace NotasOracleWinForms
 {
     public partial class Form1 : Form
     {
+        ConexionOracle conexion = new ConexionOracle();
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
