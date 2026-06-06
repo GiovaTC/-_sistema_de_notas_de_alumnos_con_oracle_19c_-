82455d9ab54a9583a208a45388cf59d087deade8
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-06 13:34:51 -0500

    commit - Form1 .

--- Form1.cs
+++ Form1.cs
@@ -15,9 +15,20 @@ namespace NotasOracleWinForms
 
         }
 
-        private void button_Click(object sender, EventArgs e)
+        private void btnCalcular_Click(object sender, EventArgs e)
         {
+            decimal n1 = decimal.Parse(txtNota1.Text);
+            decimal n2 = decimal.Parse(txtNota2.Text);
+            decimal n3 = decimal.Parse(txtNota3.Text);
+            decimal n4 = decimal.Parse(txtNota4.Text);
+            decimal n5 = decimal.Parse(txtNota5.Text);
 
+            decimal suma = n1 + n2 + n3 + n4 + n5;
+
+            decimal promedio = suma / 5;
+
+            txtSuma.Text = suma.ToString();
+            txtPromedio.Text = promedio.ToString("0.00");
         }
 
         private void Form1_Load_1(object sender, EventArgs e)
