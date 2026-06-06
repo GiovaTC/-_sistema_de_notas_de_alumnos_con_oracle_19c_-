32b2d99fe4c2e65628d744ced90a41dad9ecf3a3
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-06 13:08:18 -0500

    commit - Alumno .

---
+++ Alumno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotasOracleWinForms
+{
+    public class Alumno
+    {
+        public string Nombre { get; set; }
+
+        public decimal Nota1 { get; set; }
+        public decimal Nota2 { get; set; }
+        public decimal Nota3 { get; set; }
+        public decimal Nota4 { get; set; }
+        public decimal Nota5 { get; set; }
+
+        public decimal Suma { get; set; }
+        public decimal Promedio { get; set; }
+    }
+}   
